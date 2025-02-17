ad999c9edb19d5f310818fc78ec3bd1717ca368e
jamesbrq <jamesbrq@gmail.com>
2025-02-16 23:48:09 -0500

    Update main.cpp

--- rel/source/AP/main.cpp
+++ rel/source/AP/main.cpp
@@ -7,6 +7,12 @@ extern int32_t statusWinDisp[];
 extern int32_t winRootMain[];
 extern int32_t compare_func3_r[];
 extern int32_t compare_func3[];
+extern int32_t mapGX[];
+extern int32_t winGetMapTplName[];
+extern int32_t winLogMain[];
+extern int32_t winLogInit[];
+extern int32_t monoshiriGX[];
+extern int32_t monosiri_disp[];
 
 
 void ApplyMainAssemblyPatches()
@@ -22,6 +28,8 @@ void ApplyMainAssemblyPatches()
 	compare_func3_r[51] = 0x386006A4; // li r3, 0x6B3 (GSW(1715))
 	compare_func3_r[54] = 0x2C030005; // cmpwi r3, 0x5
 
-	compare_func3[109] = 0x386006A4; // li r3, 0x6B3 (GSW(1715))
+	compare_func3[29] = 0x386006A4; // li r3, 0x6B3 (GSW(1715))
 	compare_func3[32] = 0x2C030005; // cmpwi r3, 0x5
+	compare_func3[51] = 0x386006A4; // li r3, 0x6B3 (GSW(1715))
+	compare_func3[54] = 0x2C030005; // cmpwi r3, 0x5
 }
