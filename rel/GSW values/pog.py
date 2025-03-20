1ffb21c105dd33b4d1c03e8e471287f7679efbe4
jamesbrq <jamesbrq@gmail.com>
2025-03-20 14:30:59 -0400

    Update pog.py

--- rel/GSW values/pog.py
+++ rel/GSW values/pog.py
@@ -1,21 +1,17 @@
-instruction = 0x805F6AA8
-base = 0x805F6838
-strings = [
-  ""
-]
+instruction = 0x805E83A8
+base = 0x805E83A8
+
+offset = int((instruction - base) / 4)
+rel = 0x0000dd98
+
+
+skip = 0
 
 text = "キノシコワ"
 hex_data = text.encode('shift_jis')
 literal_hex_string = ''.join(f'\\x{byte:02X}' for byte in hex_data)
 c_literal = f'const char jolene[] = "{literal_hex_string}";'
 
-print(c_literal)
-
-split = strings[0].split(":")[1:]
-
-def print_strings():
-  for i, string in enumerate(split):
-    print(f"extern int32_t {string[:-13] if i != (len(split) - 1) else string}[];")
-
-print_strings()
-print(f"\n{int((instruction - base) / 4)}")
+#print(c_literal)
+print(f"\n{int(offset)}")
+print(f"\n0x" + f"{format(rel + ((offset + skip) * 4), 'x')}".upper())
