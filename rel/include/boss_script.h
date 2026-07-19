258ec6ea8432f48cbf39ed580ded2b95315f3259
jamesbrq <jamesbrq@gmail.com>
2026-07-19 19:05:48 -0400

    Pre-Commit

---
+++ rel/include/boss_script.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void GoldChorobonPatches(int scaledHp);
+void HooktailPatches(int scaledHp);
+void MagnumBattenderPatches(int scaledHp);
+void FakerMarioPatches(int scaledHp);
+void Kanbu3Patches(int scaledHp);
+void MagnumBattenderMKIIPatches(int scaledHp);
